a3fcad283d47509668dd7983ab5c32ca39bac91b
collin-st <collin.street.dr@gmail.com>
2018-12-07 21:56:57 -0800

    Progress towards Session Parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,24 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const Cookies = require('./cookieParser');
 
 module.exports.createSession = (req, res, next) => {
+
+  if (!req.get('Cookie')) {
+    models.Sessions.create()
+      .then((result) => {
+        return models.Sessions.get({ id: result.insertId })
+        // console.log(result);
+      })
+      .then((newResult) => {
+        // console.log(newResult);
+        req.session = { hash: newResult.hash }
+        res.cookies = { 'shortlyid': { value: newResult.hash } }
+        next();
+      })
+  } else {
+    // if cookie header does exist
+  }
 };
 
 /************************************************************/
